178f8b7852d182219d3b7dce0769718081418f71
JWLiang <961055871@qq.com>
2020-04-24 18:51:16 +0800

    update

---
+++ VariableType/VariableType.java
@@ -0,0 +1,15 @@
+public class VariableType {
+	public static void main (String[] args){
+		
+		/* 8 basic variable type totally */
+		
+		boolean boolVar = true;	// value should be either true or false
+		char charVar = 'c';	
+		byte byteVar = -27;
+		short shortVar = -215;
+		int intVar = -231;
+		long longVar = -263l;	// L-suffix is necessary for long variable
+		float floatVar = -0.32f;	// f-suffix is necessary for float variable
+		double doubleVar = -0.64d;	// equivalent to = -0.64 
+	}	
+}
